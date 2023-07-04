3e5eb1b33714b5f51934b43efcbbd17185f2e2de
BadCloudP <138548416+BadCloudP@users.noreply.github.com>
2023-07-04 14:52:06 +0800

    Add files via upload

---
+++ presamp2dvtb/presamp2dvtb/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmmain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btninput = New System.Windows.Forms.Button()
+        Me.OpenFileDialoginput = New System.Windows.Forms.OpenFileDialog()
+        Me.btnOutput = New System.Windows.Forms.Button()
+        Me.btnChange = New System.Windows.Forms.Button()
+        Me.lstInputView = New System.Windows.Forms.ListBox()
+        Me.SaveFileDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'btninput
+        '
+        Me.btninput.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.btninput.Location = New System.Drawing.Point(12, 12)
+        Me.btninput.Name = "btninput"
+        Me.btninput.Size = New System.Drawing.Size(91, 23)
+        Me.btninput.TabIndex = 4
+        Me.btninput.Text = "input file"
+        Me.btninput.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialoginput
+        '
+        Me.OpenFileDialoginput.FileName = "presamp.ini"
+        Me.OpenFileDialoginput.Filter = """ini files (*.ini)|*.ini|All files (*.*)|*.*"""
+        Me.OpenFileDialoginput.InitialDirectory = """c:/"""
+        Me.OpenFileDialoginput.RestoreDirectory = True
+        '
+        'btnOutput
+        '
+        Me.btnOutput.Location = New System.Drawing.Point(206, 12)
+        Me.btnOutput.Name = "btnOutput"
+        Me.btnOutput.Size = New System.Drawing.Size(91, 23)
+        Me.btnOutput.TabIndex = 7
+        Me.btnOutput.Text = "Output"
+        Me.btnOutput.UseVisualStyleBackColor = True
+        '
+        'btnChange
+        '
+        Me.btnChange.Location = New System.Drawing.Point(109, 12)
+        Me.btnChange.Name = "btnChange"
+        Me.btnChange.Size = New System.Drawing.Size(91, 23)
+        Me.btnChange.TabIndex = 6
+        Me.btnChange.Text = "Change"
+        Me.btnChange.UseVisualStyleBackColor = True
+        '
+        'lstInputView
+        '
+        Me.lstInputView.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lstInputView.HorizontalExtent = 500
+        Me.lstInputView.HorizontalScrollbar = True
+        Me.lstInputView.ItemHeight = 15
+        Me.lstInputView.Location = New System.Drawing.Point(12, 50)
+        Me.lstInputView.Name = "lstInputView"
+        Me.lstInputView.ScrollAlwaysVisible = True
+        Me.lstInputView.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.lstInputView.Size = New System.Drawing.Size(360, 199)
+        Me.lstInputView.TabIndex = 5
+        '
+        'SaveFileDialog
+        '
+        Me.SaveFileDialog.DefaultExt = "txt"
+        Me.SaveFileDialog.FileName = "Resultdvtb.txt"
+        Me.SaveFileDialog.Filter = """txt Files (*.txt*)|*.txt"""
+        '
+        'frmmain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.AutoScroll = True
+        Me.AutoScrollMinSize = New System.Drawing.Size(330, 220)
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(384, 261)
+        Me.Controls.Add(Me.btninput)
+        Me.Controls.Add(Me.btnOutput)
+        Me.Controls.Add(Me.btnChange)
+        Me.Controls.Add(Me.lstInputView)
+        Me.MaximumSize = New System.Drawing.Size(1000, 750)
+        Me.MinimumSize = New System.Drawing.Size(400, 300)
+        Me.Name = "frmmain"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.Text = "presamp2dvtb_aphla_0.0.1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btninput As Button
+    Friend WithEvents OpenFileDialoginput As OpenFileDialog
+    Friend WithEvents btnOutput As Button
+    Friend WithEvents btnChange As Button
+    Friend WithEvents lstInputView As ListBox
+    Friend WithEvents SaveFileDialog As SaveFileDialog
+End Class
---
+++ presamp2dvtb/presamp2dvtb/Form1.vb
@@ -0,0 +1,186 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+'by_邪云
+Public Class frmmain
+    Private Sub btninput_Click(sender As Object, e As EventArgs) Handles btninput.Click
+        Dim inputdata As String
+        lstInputView.Items.Clear()
+        '开启档案
+        If OpenFileDialoginput.ShowDialog() = DialogResult.OK Then
+            Dim sr As New System.IO.StreamReader(OpenFileDialoginput.FileName)
+            inputdata = sr.ReadToEnd
+            sr.Close()
+            Dim lstInput() = Split(inputdata, vbLf)
+            '导出list
+            For lstInput_i = 0 To lstInput.Count - 1
+                lstInputView.Items.Add(lstInput(lstInput_i))
+            Next
+        End If
+    End Sub
+
+    Private Sub frmmain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstInputView.Items.Clear()
+    End Sub
+
+    Private Sub btnChange_Click(sender As Object, e As EventArgs) Handles btnChange.Click
+
+        Dim lstInput(lstInputView.Items.Count - 1) As String
+        '导入序列
+        For lstInput_i = 0 To lstInput.Count - 1
+            lstInput(lstInput_i) = lstInputView.Items(lstInput_i)
+        Next
+        Dim CheckVowel As Boolean
+        Dim CheckCnsonant As Boolean
+        Dim inputVersion As Double
+        Dim InVowel As Boolean
+        Dim InCnsonant As Boolean
+        Dim VowelCount(2) As Integer
+        Dim CnsonantCount(2) As Integer
+        Dim HavePriority As Boolean '暂时没用，用于未来的缺失检查。
+        Dim VowelReday As Boolean
+        Dim CnsonantReday As Boolean
+        For lst_i = 0 To lstInput.Count - 1
+            '计算行数
+            If InVowel = True Then
+                VowelCount(2) += 1
+                VowelReday = True
+            ElseIf InCnsonant = True Then
+                CnsonantCount(2) += 1
+                CnsonantReday = True
+            End If
+            '寻找各个key的起点
+            If lstInput(lst_i).IndexOf("[VERSION]") <> -1 Then
+                inputVersion = lstInput(lst_i + 1)
+            ElseIf lstInput(lst_i).IndexOf("[VOWEL]") <> -1 Then
+                InVowel = True
+                VowelCount(1) = CInt(lst_i + 1)
+                CheckVowel = True
+            ElseIf lstInput(lst_i).IndexOf("[CONSONANT]") <> -1 Then
+                InVowel = False
+                InCnsonant = True
+                CnsonantCount(1) = CInt(lst_i + 1)
+                CheckCnsonant = True
+            ElseIf lstInput(lst_i).IndexOf("[APPEND]") <> -1 Then
+                InCnsonant = False
+            ElseIf lstInput(lst_i).IndexOf("[PRIORITY]") <> -1 Then
+                InCnsonant = False
+                HavePriority = True
+                Dim inputPriority As String '暂时没用，用于未来的缺失检查。
+                inputPriority = lstInput(lst_i + 1)
+                Dim Priority() = Split(inputPriority, ",")
+            ElseIf lstInput(lst_i).IndexOf("[REPLACE]") <> -1 Then
+                InCnsonant = False
+            End If
+        Next
+        If VowelReday And CnsonantReday Then
+            '处理部分
+            Dim ResultAll As String = "[SymbolList]" & vbLf
+            Dim inputVowelarray(VowelCount(2) - 2) As String
+            Dim inputCnsonantarray(CnsonantCount(2) - 2) As String
+            Dim CorV_i As Integer = 0
+            '分开VC序列
+            For inputVowelarray_i = VowelCount(1) To VowelCount(1) + inputVowelarray.Count - 1
+                inputVowelarray(CorV_i) = lstInput(inputVowelarray_i)
+                CorV_i += 1
+            Next
+            CorV_i = 0
+            For inputCnsonantarray_i = CnsonantCount(1) To CnsonantCount(1) + inputCnsonantarray.Count - 1
+                inputCnsonantarray(CorV_i) = lstInput(inputCnsonantarray_i)
+                CorV_i += 1
+            Next
+            '分开每一列
+            Dim TempArray(3) As String
+            Dim Vowelarray0(inputVowelarray.Count - 1) As String
+            Dim Vowelarray1(inputVowelarray.Count - 1) As String
+            Dim Vowelarray2(inputVowelarray.Count - 1) As String
+            Dim Vowelarray3(inputVowelarray.Count - 1) As Integer
+            Dim Cnsonantarray0(inputCnsonantarray.Count - 1) As String
+            Dim Cnsonantarray1(inputCnsonantarray.Count - 1) As String
+            Dim Cnsonantarray2(inputCnsonantarray.Count - 1) As Integer
+            For Vowelarray_i = 0 To inputVowelarray.Count - 1
+                TempArray = Split(inputVowelarray(Vowelarray_i), "=")
+                Vowelarray0(Vowelarray_i) = TempArray(0)
+                Vowelarray1(Vowelarray_i) = TempArray(1)
+                Vowelarray2(Vowelarray_i) = TempArray(2)
+                Vowelarray3(Vowelarray_i) = TempArray(3)
+            Next
+            For Cnsonant_i = 0 To inputCnsonantarray.Count - 1
+                TempArray = Split(inputCnsonantarray(Cnsonant_i), "=")
+                Cnsonantarray0(Cnsonant_i) = TempArray(0)
+                Cnsonantarray1(Cnsonant_i) = TempArray(1)
+                Cnsonantarray2(Cnsonant_i) = TempArray(2)
+            Next
+            '组合起来
+            For Cnsonant_1_i = 0 To inputCnsonantarray.Count - 1
+                Dim Cnsonant_1_Input() = Split(CStr(Cnsonantarray1(Cnsonant_1_i)), ",")
+                For Cnsonant_1_Input_i = 0 To Cnsonant_1_Input.Count - 1
+                    For Vowel_2_i = 0 To inputVowelarray.Count - 1
+                        Dim Vowel_2_Input() = Split(CStr(Vowelarray2(Vowel_2_i)), ",")
+                        For Vowel_2_Input_i = 0 To Vowel_2_Input.Count - 1
+                            If Cnsonant_1_Input(Cnsonant_1_Input_i) = Vowel_2_Input(Vowel_2_Input_i) Then
+                                ResultAll += Cnsonant_1_Input(Cnsonant_1_Input_i) & "," & Cnsonantarray0(Cnsonant_1_i) & "," & Vowelarray0(Vowel_2_i) & vbLf
+                            End If
+                        Next
+
+
+                    Next
+
+                Next
+            Next
+            lstInputView.Items.Clear()
+            '输出
+            ResultAll += "[VowelList]" & vbLf
+            For Vowel_list_i = 0 To inputVowelarray.Count - 1
+                Dim TempVowellist() = Split(Vowelarray2(Vowel_list_i), ",")
+                ResultAll += Vowelarray0(Vowel_list_i) & "," & TempVowellist(0) & vbLf
+            Next
+            ResultAll += "[Cnsonant = 1]" & vbLf
+            For Cnsonant_list1_i = 0 To inputCnsonantarray.Count - 1
+                If Cnsonantarray2(Cnsonant_list1_i) = 1 Then
+                    ResultAll += Cnsonantarray0(Cnsonant_list1_i) & vbLf
+                End If
+            Next
+            ResultAll += "[Cnsonant = 0]" & vbLf
+            Dim IsinVowel As Boolean = False
+            For Cnsonant_list0_i = 0 To inputCnsonantarray.Count - 1
+                If Cnsonantarray2(Cnsonant_list0_i) = 0 Then
+                    For vowelarray_i = 0 To inputVowelarray.Count - 1
+                        '防止V部分重复
+                        If Cnsonantarray1(Cnsonant_list0_i).IndexOf(Vowelarray0(vowelarray_i)) = 0 Then
+                            IsinVowel = True
+                        End If
+                    Next
+                    If IsinVowel = True Then
+                        IsinVowel = False
+                    Else
+                        ResultAll += Cnsonantarray0(Cnsonant_list0_i) & vbLf
+                    End If
+                End If
+            Next
+            Dim lstoutput() = Split(ResultAll, vbLf)
+            'refill the list
+            For lstoutput_i = 0 To lstoutput.Count - 1
+                lstInputView.Items.Add(lstoutput(lstoutput_i))
+            Next
+        End If
+    End Sub
+
+    Private Sub btnOutput_Click(sender As Object, e As EventArgs) Handles btnOutput.Click
+        Dim IsChanged As Boolean = False
+        If lstInputView.Items(0).IndexOf("[SymbolList]") <> -1 Then
+            IsChanged = True
+        End If
+        '导出文件
+        If IsChanged = True Then
+            Dim ResultAll As String = ""
+            For lstInput_i = 0 To lstInputView.Items.Count - 1
+                ResultAll += lstInputView.Items(lstInput_i) & vbLf
+            Next
+            If SaveFileDialog.ShowDialog = DialogResult.OK Then
+                My.Computer.FileSystem.WriteAllText(SaveFileDialog.FileName, ResultAll, True)
+            End If
+        End If
+    End Sub
+
+End Class
